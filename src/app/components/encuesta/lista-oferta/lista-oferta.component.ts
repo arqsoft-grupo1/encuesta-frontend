45b0800635b148f4cb9c01f1aca0deed8e92e1f1
Martin Ramos <marprg@gmail.com>
2017-11-01 23:38:15 -0300

    Separa entre materias aprobadas, sugeridas y generales

--- src/app/components/encuesta/lista-oferta/lista-oferta.component.ts
+++ src/app/components/encuesta/lista-oferta/lista-oferta.component.ts
@@ -15,16 +15,21 @@ export class ListaOfertaComponent implements OnInit {
     materias_aprobadas;
     tmp_mat = [];
 
+    /*
+        Genera 3 variables a mostrar en distintos paneles.
+        materias_aprobadas: Viene por webservice con un flag "aprobada" que es un booleano
+        materias_sugeridas: Cada materia que tiene el flag aprobada en true tiene un campo es_correlativa_de,
+                            esto significa que es una lista de materias que podrían suceder a esta materia aprobada
+        materias: Es la lista de materias que no se incluyen ni aprobadas ni sugeridas, de esta manera evitar informacion
+                  duplicada.
+    */
     constructor(private ofertaService: OfertaService) {
         ofertaService.getOferta().subscribe(
             data => {
-             //  console.log(data);
-             //  console.log(data['oferta']);
                   this.oferta = new Oferta(data['oferta']);
-                 //  console.log("La oferta en si:" +  this.oferta.getMaterias());
                   this.materias = this.oferta.getMaterias();
-                  this.materias_aprobadas = this.materias.filter(this.materiaAprobada);
-                  this.materias = this.materias.filter(this.materiaACursar);
+                  this.materias_aprobadas = this.materias.filter(x => x['aprobada']);
+                  this.materias = this.materias.filter(x => !x['aprobada']);
                   this.generarMateriasSugeridas();
             },
             err => {
@@ -32,43 +37,46 @@ export class ListaOfertaComponent implements OnInit {
             }
         );
     }
-
+    /*
+        Evalua UNICAMENTE en la variable materias_aprobadas, de esta manera aseguramos que no se
+        sugieran materias correlativas sin haber aprobado materias anteriores. Si es que posee correlativas
+        a estas las agrega en la variable materias_sugeridas.
+    */
     generarMateriasSugeridas() {
-        //console.log(this.materias_aprobadas.length);
         for(var i = 0; i<this.materias_aprobadas.length; i++) {
             if (!(this.materias_aprobadas[i]['es_correlativa_de'] === undefined)) {
-                 //console.log(this.tmp_mat);
                 this.agregarMateriasSugeridas(this.materias_aprobadas[i]['es_correlativa_de']);
-                //console.log(this.materias_sugeridas);
             }
 
         }
-        //console.log(this.materias_aprobadas);
     }
-
+    /*
+        Si ya existe dentro de las materias sugeridas no la toma en cuenta
+        Tampoco la toma en cuenta si es correlativa pero ya esta aprobada
+        Agrega la materia en la lista de materias_sugeridas y para evitar duplicaciones
+        la saca de la lista de materias restantes.
+    */
     agregarMateriasSugeridas(materias) {
         for(var i = 0; i<materias.length; i++) {
-            if (!(this.materias_sugeridas.some(x => x === materias[i]))) {
+            if (!(this.existeMateriaSugeridaYa(materias[i]['id'])) && !(this.tieneMateriaAprobada(materias[i]['id']))) {
                 this.materias_sugeridas.push(materias[i]);
+                // Quito la materia de la lista de materias generales para que no este duplicada
+                this.eliminarMateriaDeMateriasGenerales(materias[i]['id']);
+
             }
         }
     }
 
-    existeMateria(element, index, array) {
-        console.log(element, index, array);
-        return true;
+    tieneMateriaAprobada(materiaId) {
+        return this.materias_aprobadas.some(x => x['id'] === materiaId);
     }
 
-    materiaAprobada(element, index, array) {
-        return (element['aprobada']);
+    existeMateriaSugeridaYa(materiaId) {
+        return this.materias_sugeridas.some(x => x['id'] === materiaId);
     }
 
-    materiaACursar(element, index, array) {
-        return (!element['aprobada']);
-    }
-
-    materiasSugeridas() {
-
+    eliminarMateriaDeMateriasGenerales(materiaId) {
+        this.materias = this.materias.filter(x => x['id'] !== materiaId);
     }
 
     ngOnInit() {
